97b5fb915c9dc2251891a3f7b503f7cd6c59632a
Martialcart <janolav91@hotmail.com>
2025-01-13 10:48:11 +0100

    3d plotly works

--- script.js
+++ script.js
@@ -18,3 +18,75 @@ new Chart(ctx, {
 	}
     }
 });
+
+const tester = document.getElementById('tester');
+Plotly.newPlot( tester, [{
+    x: [1,2,3,4,5],
+    y: [1,2,3,4,5] }], {
+	margin: {t: 0 }});
+
+
+//ribon plot
+
+const myDiv = document.getElementById('myDiv');
+
+  var trace1 = {
+      x:[0,0,0,0,0], y:[0,1,2,3,4], z:[[2,4],[2,6],[7,3],[4,1],[3,10]],
+    name: 'poop',
+    type: 'surface',
+    showscale: false
+  }
+  var trace2 = {
+      x:[1,1,1,1,1], y:[0,1,2,3,4], z:[1,2,7,4,3],
+    name: '',
+    type: 'surface',
+    showscale: false
+  }
+  var trace3 = {
+      x:6, y:2, z:2,
+    name: '',
+    type: 'surface',
+    showscale: false
+  }
+  var trace4 = {
+      x:3, y:3, z:3,
+    name: '',
+    type: 'surface',
+    showscale: false
+  }
+  var trace5 = {
+      x:7, y:4, z:4,
+    name: '',
+    type: 'surface',
+    showscale: false
+  }
+  var trace6 = {
+      x:2, y:5, z:5,
+    name: '',
+    type: 'surface',
+    showscale: false
+  }
+  var trace7 = {
+      x:6, y:6, z:6,
+    name: '',
+    type: 'surface',
+    showscale: false
+  }
+
+  var data = [trace1, trace2];
+
+  var layout = {
+    title: {
+      text: 'Ribbon Plot'
+    },
+    showlegend: false,
+    autosize: true,
+    width: 600,
+    height: 600,
+    scene: {
+      xaxis: {title: {text: 'Sample #'}},
+      yaxis: {title: {text: 'Wavelength'}},
+      zaxis: {title: {text: 'OD'}}
+    }
+  };
+  Plotly.newPlot(myDiv, data, layout);
